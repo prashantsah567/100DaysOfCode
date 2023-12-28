20fc80edd4715d7e4cb5076bbdbc00b8bde0733c
Prashant Sah <prashantsah567@gmail.com>
2023-12-28 18:55:06 -0500

    Day 46 Same Tree challenge solved - 2

---
+++ Day46SameTree.java
@@ -0,0 +1,34 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Day46SameTree {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        //base case to check if both p and q are null or not
+        if(p == null && q == null) return true;
+        else if(p == null || q == null) return false;
+        //check the values and recurse on left and right subtrees
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        System.out.println(isSameTree(root, root));
+    }
+}
